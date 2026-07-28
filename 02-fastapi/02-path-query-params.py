b1019bd1a66b758371641b0c244a6ff08a214ad5
Rohit Karthick <rkbeo5@gmail.com>
2026-07-28 11:39:36 +0530

    implement path and query params apis

--- 02-fastapi/02-path-query-params.py
+++ 02-fastapi/02-path-query-params.py
@@ -14,6 +14,18 @@ all_todos = [
 def index():
     return {"message": "Hello World"}
 
-@app.get('/todos')  # static route
-def get_all_todos():
+# @app.get('/todos')  # static route
+# def all_todos():
+#     return all_todos
+
+@app.get('/todo/{todo_id}') # path params
+def get_todo(todo_id: int):
+    for todo in all_todos:
+        if todo['todo_id'] == todo_id:
+            return {"result": todo}
+
+@app.get('/todos')  # query params. Run with http://127.0.0.1:9999/todos?first_n=3
+def all_todos(first_n: int = None):
+    if first_n:
+        return all_todos[:first_n]
     return all_todos
